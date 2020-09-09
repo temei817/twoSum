4c81764a0f6567243c95276dcbaa8edfaf36e4c0
temei817 <40670113+temei817@users.noreply.github.com>
2020-09-09 15:46:38 -0700

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -9,15 +9,15 @@ public class twoSum{
 	
 		for(int i = 0; i < list.length-1; i++)
 		{
-			min = i;
+			min = i; //sets index
 			for(int n = i + 1; n < list.length; n++)
 			{	
 				if(list[n] < list[min])
 				{
-					min = n;
+					min = n; //finds the smallest num
 				}
 			}
-			temp = list[min];
+			temp = list[min]; //swaps value
 			list[min] = list[i];
 			list[i] = temp;
 		
@@ -30,19 +30,19 @@ public class twoSum{
 		int end = sum.length-1;
 		while(start < end)
 		{	
-			if(((sum[start] + sum[end]) == num) && (sum[start] != sum[end]))
+			if(((sum[start] + sum[end]) == num) && (sum[start] != sum[end])) //checks if start + end = num and makes sure they're not the same number, returns index of start and end
 			{
 				return new int[] {start, end};
 			}
-			else if(((sum[start] + sum[end]) == num) && (sum[start] == sum[end]))
+			else if(((sum[start] + sum[end]) == num) && (sum[start] == sum[end])) //if it is a duplicate, returns [-1,-1]
 			{
 				return new int[] {-1, -1};
 			}
-			else if((sum[start] + sum[end]) > num)
+			else if((sum[start] + sum[end]) > num) //if the sum is bigger than target, move end pointer one to the left
 			{
 				end--;
 			}
-			else if((sum[start] + sum[end]) < num)
+			else if((sum[start] + sum[end]) < num) //if the sum is smaller than target, move start pointer one to the right
 			{
 				start++;
 			}
